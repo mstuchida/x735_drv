1e73e82136501054b1264d0132066acac049c476
Masato Uchida <masato@gmail.com>
2021-01-24 04:47:46 +0900

    GPIO18 was used to turn off the power

--- etc/poweroff.sh
+++ etc/poweroff.sh
@@ -9,4 +9,11 @@ echo 0 > /sys/class/gpio/gpio17/value
 sleep 3
 echo 1 > /sys/class/gpio/gpio17/value
 
+sleep 3
+echo 18 > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio18/direction
+echo 1 > /sys/class/gpio/gpio18/value
+/bin/sleep 5
+echo 0 > /sys/class/gpio/gpio18/value
+
 /bin/systemctl poweroff
