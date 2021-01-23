e9f09af328a63eba607e0e20ac10c95c20ed5cad
Masato Uchida <masato@gmail.com>
2021-01-24 03:07:42 +0900

    Added the process of sending shutdown messages

--- etc/poweroff.sh
+++ etc/poweroff.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-sleep 5
+wall -n -t 3 "Shutting down the system."
+
+sleep 3
 echo 17 > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio17/direction
 echo 0 > /sys/class/gpio/gpio17/value
-sleep 5
+sleep 3
 echo 1 > /sys/class/gpio/gpio17/value
 
 /bin/systemctl poweroff
