d092dec6dd0cb9d372241e7347fd341554784ee6
Masato Uchida <masato@gmail.com>
2021-01-24 07:58:59 +0900

    Code optimization

--- driver/x735.c
+++ driver/x735.c
@@ -9,17 +9,75 @@
 #define DEVICE_NAME "x735"
 
 /* functions */
-static irqreturn_t gpio_handler(int irq, void *arg);
 static int gpio_init(void);
+static int regist_interrupt_handler(void);
+static irqreturn_t interrupt_handler(int irq, void *arg);
 static void gpio_exit(void);
 
 /* global varibles */
 spinlock_t *lock;
 static struct class *gpio_class;
 static bool is_processed = false;
+static struct gpio gpios[] = {
+    {GPIO4, GPIOF_DIR_IN, DEVICE_NAME},
+    {GPIO17, GPIOF_OUT_INIT_HIGH, DEVICE_NAME}
+};
 
-/* interrupt function */
-static irqreturn_t gpio_handler(int irq, void *arg) {
+/* initialize funtion */
+static int __init gpio_init(void) {
+
+    int ret;
+
+    // create class under /sys/class
+    gpio_class = class_create(THIS_MODULE, DEVICE_NAME);
+    if (gpio_class == NULL) {
+        printk(KERN_ERR "can't create class.\n");
+        return -EINVAL;
+    }
+
+    // Initial configuration of gpio request
+    ret = gpio_request_array(gpios, ARRAY_SIZE(gpios));
+    if (ret != 0) {
+        printk(KERN_ERR "failed to request gpio err_code=%d\n", ret);
+        return -ENODEV;
+    }
+
+    lock = kmalloc(sizeof(spinlock_t), GFP_KERNEL);
+    if (lock == NULL) {
+        printk("failed to kmalloc.\n");
+        return -ENOMEM;
+    }
+
+    // spinlock guarantees exclusive control using busy wait locking
+    spin_lock_init(lock);
+
+    // registering an interrupt handler
+    ret = regist_interrupt_handler();
+    if (ret != 0) {
+        printk(KERN_ERR "failed to regist interrupt handler. err_code=%d\n", ret);
+        return ret;
+    }
+
+    printk("initialized %s driver.\n", DEVICE_NAME);
+    return 0;
+}
+
+/* registering handler function */
+static int regist_interrupt_handler(void) {
+
+    int ret;
+    int irq;
+    unsigned long flags;
+
+    irq = gpio_to_irq(GPIO4);
+    spin_lock_irqsave(lock, flags);
+    ret = request_irq(irq, interrupt_handler, IRQF_SHARED | IRQF_TRIGGER_RISING, DEVICE_NAME, lock);
+    spin_unlock_irqrestore(lock, flags);
+    return ret;
+}
+
+/* interrupt handler function */
+static irqreturn_t interrupt_handler(int irq, void *arg) {
 
     unsigned long flags;
     static char *argv[] = {
@@ -33,82 +91,21 @@ static irqreturn_t gpio_handler(int irq, void *arg) {
         return IRQ_HANDLED;
     }
 
-    // disabled interrupt
-    local_irq_save(flags);
     printk(KERN_INFO "process gpio_handler.\n");
 
-    // release gpio
-    gpio_free(GPIO4);
-    gpio_free(GPIO17);
-
-    // power off
+    local_irq_save(flags);
+    gpio_free_array(gpios, ARRAY_SIZE(gpios));
     call_usermodehelper(argv[0], argv, NULL, UMH_NO_WAIT);
-
-    // enabled interrupt
     local_irq_restore(flags);
-
     is_processed = true;
-
     return IRQ_HANDLED;
-}
-
-/* initialize funtion */
-static int __init gpio_init(void) {
-
-    int ret;
-    int irq;
-    unsigned long flags;
-
-    // create class under /sys/class
-    gpio_class = class_create(THIS_MODULE, DEVICE_NAME);
-    if (gpio_class == NULL) {
-        printk(KERN_ERR "can't create class.\n");
-        return -EINVAL;
-    }
-
-    lock = kmalloc(sizeof(spinlock_t), GFP_KERNEL);
-    if (!lock) {
-        printk("failed to kmalloc.\n");
-        return -ENOMEM;
-    }
-
-    // Initial configuration of single GPIO4/17/18 request
-    ret = gpio_request_one(GPIO4, GPIOF_DIR_IN, DEVICE_NAME);
-    if (ret != 0) {
-        printk(KERN_ERR "failed to request gpio4 err_code=%d\n", ret);
-        return -ENODEV;
-    }
-    ret = gpio_request_one(GPIO17, GPIOF_OUT_INIT_HIGH, DEVICE_NAME);
-    if (ret != 0) {
-        printk(KERN_ERR "failed to request gpio17 err_code=%d\n", ret);
-        return -ENODEV;
-    }
-
-    // spinlock guarantees exclusive control using busy wait locking
-    spin_lock_init(lock);
-
-    // open gpio4
-    irq = gpio_to_irq(GPIO4);
-    spin_lock_irqsave(lock, flags);
-    ret = request_irq(irq, gpio_handler, IRQF_SHARED | IRQF_TRIGGER_RISING, DEVICE_NAME, lock);
-    spin_unlock_irqrestore(lock, flags);
-    if (ret != 0) {
-        printk(KERN_ERR "failed to set interrupt. irq=%d, err_code=%d\n", irq, ret);
-        return ret;
-    }
-
-    printk("initialized %s driver.\n", DEVICE_NAME);
-    return 0;
 }
 
 /* exit function */
 static void __exit gpio_exit(void) {
 
     kfree(lock);
-    gpio_direction_output(GPIO4, 0);
-    gpio_direction_output(GPIO17, 0);
-    gpio_free(GPIO4);
-    gpio_free(GPIO17);
+    gpio_free_array(gpios, ARRAY_SIZE(gpios));
     class_destroy(gpio_class);
     printk(KERN_INFO "removed %s driver.\n", DEVICE_NAME);
 }
@@ -116,7 +113,7 @@ static void __exit gpio_exit(void) {
 module_init(gpio_init);
 module_exit(gpio_exit);
 
-MODULE_VERSION("0.1.1");
+MODULE_VERSION("0.1.2");
 MODULE_AUTHOR("mst.uchida");
 MODULE_DESCRIPTION("unofficial driver for x735");
 MODULE_LICENSE("GPL v2");
